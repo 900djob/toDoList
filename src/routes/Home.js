1b035203dd9dc510cd019b78840ea183257133bd
seok <dreaming.seok@gmail.com>
2021-09-29 22:13:46 +0900

    :construction: mapStateToProps

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,24 +1,29 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
 
-function Home() {
-    const [text, setText] = useState('');
-    function onChange(e) {
-        setText(e.target.value)
-    } 
-    function onSubmit(e) {
-        e.preventDefault();
-        setText("");
-    }
+function Home({ toDos }) {
+  const [text, setText] = useState("");
+  function onChange(e) {
+    setText(e.target.value);
+  }
+  function onSubmit(e) {
+    e.preventDefault();
+    setText("");
+  }
   return (
     <>
       <h1>To Do List</h1>
       <form onSubmit={onSubmit}>
-          <input type='text' value={text} onChange={onChange} />
-          <button>Add</button>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   );
 }
 
-export default Home;
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+
+export default connect(mapStateToProps)(Home);
